793039f525aea3235d60cdfd0ed77286785b1dff
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2024-12-11 15:25:54 +0200

    deleted not needed files

--- main.cpp
+++
@@ -1,9 +0,0 @@
-// #include "one_big_lib.hpp"
-// int main ()
-// {
-//     std::cout << "BINGDING" << std::endl;
-//     Binding bs(AF_INET, SOCK_STREAM, 0 , 81, INADDR_ANY);
-//     std::cout << "LISTENING" << std::endl;
-//     Listen ls(AF_INET, SOCK_STREAM, 0 , 80, INADDR_ANY, 10);
-//     std::cout << "All goood" << std::endl;
-// }
