99c813e9285c9523c0b18c39fe6106aae1636851
Alice Li <aliceli.bls@gmail.com>
2024-12-31 13:53:03 +0200

    finished initializing server

--- src/ParseConfig.cpp
+++ src/ParseConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: alli <alli@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 13:15:03 by alli              #+#    #+#             */
-/*   Updated: 2024/12/30 14:57:11 by alli             ###   ########.fr       */
+/*   Updated: 2024/12/31 13:51:00 by alli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,29 +32,54 @@ void ParseConfig::splitConfigElements(const std::string configFile)
 	{
 		if (element.find("[server]") != std::string::npos)
 		{
-			_serverConfig[currentConfig.server].push_back(currentConfig); //creates new server in map
+			if (!currentConfig.server.empty())
+				_serverConfig[currentConfig.server].push_back(currentConfig); //creates new server in map
 			currentConfig = configServer(); //resets for the next server
 			if (element.find("port"))
 			{
+				//check if there is already a port
 				std::vector<std::string> tokens = Utils::splitStr(element, "=");
 				if (tokens.size() == 2)
 					currentConfig.port = tokens[1];
 			}
 			else if (element.find("ipAddress"))
 			{
+				//check if there is already an IPaddress
 				std::vector<std::string> tokens = Utils::splitStr(element, "="); //uses local scope
 				if (tokens.size() == 2)
 					currentConfig.ipAddress = tokens[1];
+				std::cout << currentConfig.ipAddress << std::endl;
 			}
-			//repeat for all structs
+			else if(element.find("server_name"))
+			{
+				std::vector<std::string> tokens = Utils::splitStr(element, "=");
+				if (tokens.size() == 2)
+					currentConfig.server_name = tokens[1];
+			}
+			else if (element.find("methods"))
+			{
+				std::vector<std::string> tokens = Utils::splitStr(element, "=");
+				if (tokens.size() >= 2 && tokens.size() <= 4)
+				{
+					if (tokens.size() == 2)
+						currentConfig.methods.push_back(tokens[1]);
+					else
+					{
+						currentConfig.methods = Utils::splitStr(tokens[1], " "); // some more parsing here to make sure it's GET POST and DELETE
+						std::cout << currentConfig.methods << std::endl;
+					}
+				}
+			}
+			if (element.empty())
+				return;
 			//find location here
-			
 		}
-		currentConfig.server.empty()
-		// keyValues = Utils::splitStr(_elements[i], "=");
-		//split into key and value
-		//if key = server, save to struct?
-		//if port then 8080 
-		//
+		if (!currentConfig.server.empty())
+			_serverConfig[currentConfig.server].push_back(currentConfig);
 	}
+}
+
+int main(int argv, char **arv)
+{
+	
 }
